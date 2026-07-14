cd63f57dbbfe1bf761932a00d1f0ea012a8bbe17
Shivam <shivamgarade05@gmail.com>
2026-07-14 21:24:00 +0530

    refactor: organize variable declarations and improve console output

--- 01_variables.js
+++ 01_variables.js
@@ -1,1 +1,20 @@
 // start of javascript
+
+const accountId = 1232323
+let accountEmail = "s@s.com"
+var accountPassword = "121232"
+accountCity = "Gondia"
+let accountState;
+
+
+// console.log(accountId,accountEmail,accountPassword,accountCity) - instead of this will do this
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+{
+    // this is scope 
+}
+
+// one developer do make variable
+// another developer knowing or by not knowing can change that so
+
