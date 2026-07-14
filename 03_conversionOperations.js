abfa67489b11cda25e8ba47864c00aad94f3ad87
Shivam <shivamgarade05@gmail.com>
2026-07-14 22:08:23 +0530

    feat: add conversion operations examples and console outputs

---
+++ 03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = "33abc";
+// let score = null;// 0
+let score = true;// 1
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN - typeof this is number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shivam" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
