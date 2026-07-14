c2b629d4e6174c324c6010ca9158aafd211a97f4
Shivam <shivamgarade05@gmail.com>
2026-07-14 21:48:43 +0530

    feat: add initial data types examples and comments

---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+// "use strict" - eariler need to write it but not now
+
+// alert (3+3) // we are using nodejs, not browser
+
+// console.log(3
+// +3 // this is not good practice
+// ) console.log("shivam") - this not readable and not valid also
+
+
+let name = "shivam" // string
+let age = 22 // number
+let isLoggedIn = false // boolean
+// bigint
+// undefined - not defined value is not assisgned
+// symbol - unique characters
+// null - standalone value
+
+// typeof - to get the type of any value
+// typeof(null) - object -- interview question
